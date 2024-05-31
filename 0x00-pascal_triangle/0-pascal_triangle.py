c070fd2a5eda068a620b88ca683160f53395e22a
abdoomr2003 <abdoomer1112003@gmail.com>
2024-05-31 04:51:09 +0300

    A

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python3
-"""
-Module 0-pascal_triangle.py
-In this function it will generate pascal triangle
-"""
-
-
-def fact(numb):
-    """this method will return the factorial of the number"""
-    if numb == 0 or numb == 1:
-        return 1
-    else:
-        pro = 1
-        for i in range(1, numb + 1):
-            pro *= i
-        return pro
-
-
 def pascal_triangle(n):
     try:
         if n <= 0:
