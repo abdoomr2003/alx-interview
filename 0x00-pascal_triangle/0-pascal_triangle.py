021c622431e8b7532902ab8ad6e0890c75c72f80
abdoomr2003 <abdoomer1112003@gmail.com>
2024-05-31 04:55:20 +0300

    A

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 def pascal_triangle(n):
-    try:
-        if n <= 0:
-            return []
-        else:
-            lists = []
-            for i in range(n):
-                row = []
-                for j in range(i + 1):
-                    row.append(fact(i) // (fact(j) * fact(i - j)))
-                lists.append(row)
-            return lists
-    except Exception:
+    """
+    Generate Pascal's Triangle up to n rows.
+
+    :param n: Number of rows in Pascal's Triangle
+    :type n: int
+    :return: List of lists representing Pascal's Triangle
+    :rtype: list
+    """
+    if n <= 0:
         return []
+    triangle = [[1]]  # Start with the first row
+    for i in range(1, n):
+        # Start each row with '1'
+        row = [1]
+        # Generate the middle values
+        k = i-1
+        row.extend([triangle[k][j-1] + triangle[k][j] for j in range(1, i)])
+        # End each row with '1'
+        row.append(1)
+        # Append the generated row to the triangle
+        triangle.append(row)
+
+    return triangle
